669449558533573c6504ca842f9499014e418a9a
Wout Philipsen <phlpsn@Wouts-MacBook-Air.local>
2026-03-11 09:05:26 +0100

    main: added spring boot application

---
+++ campus/src/test/java/be/ucll/examination/campus/CampusApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.examination.campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
